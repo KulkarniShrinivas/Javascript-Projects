038193996368e9ae301dbba95e7e83e9d916fc29
Shrinivas Kulkarni <shrinivaskulkarni989@gmail.com>
2024-01-23 23:57:15 +0530

    updated

--- Simple-Calculator/script.js
+++ Simple-Calculator/script.js
@@ -1,5 +1,32 @@
 (function () {
+    //taking all the variable
     let screen = document.querySelector('.screen');
-    
+    let buttons = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+
+    //we need to make all the variables working and 
+    //will create functon whenever user clicks number it displays
+    //event listener which button is clicked
+
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(e) {
+            let value = e.target.dataset.num;
+            screen.value += value;
+        })
+    });
+
+    equal.addEventListener('click', function(e){
+        if(screen.value === ''){
+            screen.value = "pleasse";
+        } else {
+            let answer = eval(screen.value);
+            screen.value = answer;
+        }
+    })
+
+    clear.addEventListener('click', function(e){
+        screen.value = "";
+    })
 
 })();
